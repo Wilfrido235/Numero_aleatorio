907fc3a6c07a5262334ba13544ea0b501450d56d
Eric20011 <ewjimenez@tecnologicoloja.edu.ec>
2021-07-16 22:13:50 -0500

    Version 3

--- ValidarNumeroIngresado-master/app/src/main/java/com/example/numeroalazar/MainActivity.java
+++ ValidarNumeroIngresado-master/app/src/main/java/com/example/numeroalazar/MainActivity.java
@@ -13,16 +13,18 @@ import androidx.appcompat.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity {
     private Button validarRespuesta;
     private EditText numeroU;
-
     private int numR;
-    int valorC;
+    int valorC = 0;
+    private EditText random;
+    private int randomFin;
+    //int a= Integer.parseInt(null);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         // Union xml
 
-        numR=crearAleatorio();
-        System.out.println("Aleatorio"+crearAleatorio());
+        //numR=crearAleatorio();
+        System.out.println("Aleatorio"+numRandom());
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.inicio);
@@ -32,12 +34,24 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                   numeroU= findViewById(R.id.etNumber);
-                 String num = numeroU.getText().toString();
-                  valorC = Integer.parseInt(num);
+                  random=findViewById(R.id.etNumberRandom);
 
-                Intent i = new Intent(MainActivity.this, palatially.class);
-                i.putExtra("Usuario",valorC);
-                startActivity(i);
+                randomFin=Integer.parseInt(devovernumRandomString());
+                  valorC=devolverNumIngresado();
+                  int randomEnvio = numRandom();
+
+                  if (valorC !=0 && randomFin !=0){
+                      if(!validarRango(randomFin,valorC)){
+                          numeroU.setError("Numero no esta en el rango");
+                      }else{
+                          Intent i = new Intent(MainActivity.this, palatially.class);
+                          i.putExtra("Usuario",valorC);
+                          i.putExtra("Random",randomEnvio);
+                          startActivity(i);
+                      }
+                  }else{ }
+
+
                 //System.out.println("Nuemro Usuario"+valorC);
 
 
@@ -68,8 +82,42 @@ public class MainActivity extends AppCompatActivity {
         super.onDestroy();
     }
 
-    public int crearAleatorio(){
-     return (int) (Math.random()*10);
+    public String devovernumRandomString(){
+        String randonvalor;
+        if(random.getText().toString().isEmpty()){
+            random.setError("Ingresa un numero");
+            return "0";
+        }else {
+            return randonvalor= random.getText().toString();
+        }
+    }
+
+
+    public int devolverNumIngresado(){
+
+        if (numeroU.getText().toString().isEmpty()){
+            numeroU.setError("Ingresa un numero");
+            return 0;
+        }else {
+            String num = numeroU.getText().toString();
+            return valorC = Integer.parseInt(num);
+        }
+
+    }
+
+    public int numRandom(){
+
+        return (int) (Math.random() * randomFin) + 1;
+    }
+
+    public boolean validarRango(int a,int b){
+        boolean valor = false;
+        if (a < b){
+            return valor;
+        }else{
+            return valor=true;
+        }
+
     }
 
 
